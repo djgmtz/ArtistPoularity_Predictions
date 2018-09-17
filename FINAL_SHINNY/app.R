7f3019d2e7d1d2ec2c3e41d3784aedce3063127c
Diego GM <34012705+djgmtz@users.noreply.github.com>
2018-09-16 22:44:58 -0400

    fixed

--- FINAL_SHINNY/app.R
+++ FINAL_SHINNY/app.R
@@ -11,20 +11,20 @@ library(ggplot2)
 library(DT)
 
 
-top_artists_new<- as.list(numFollowers$artist_name)
+top_track_data<- as.list(trackOutput_analysis$track_name)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Artist Popularity"),
+  titlePanel("Track Popularity"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      selectInput("Select Artist",label="Artist Selected:",
-                  choices= top_artists_new
+      selectInput("Select Track",label="Track Selected:",
+                  choices= top_track_data
       ),
-      selected=top_artists_new[1]
+      selected=top_track_data[1]
       
     ),
     
@@ -37,21 +37,21 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
+
 server <- function(input, output) {
   
-  attach(numFollowers)
+  ###MOST STREAMED
+  attach(track_monthoverview)
   output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    numFollowers$artist_name <- factor(numFollowers$artist_name, levels = numFollowers$artist_name)
+    # generate bins based on input$bins from ui.
     
+    track_monthoverview$track_name  <- factor(track_monthoverview$track_name , levels = track_monthoverview$track_name [order(track_monthoverview$totalstreams )]) # to visualise the list in descending order 
     
-    numFollowers$artist_name <- factor(numFollowers$artist_name, levels = numFollowers$artist_name[order(numFollowers$artist_num_followers)]) # to visualise the list in descending order 
-    
-    ggplot(numFollowers, aes(x = numFollowers$artist_name, y = numFollowers$artist_num_followers)) +
-      geom_bar(stat = "identity",  fill = "tomato2", width = 0.6 ) + 
-      labs(title = "Top Artists by popularity", x = "Artists", y = "Spotify Popularity Ranking") +
+    ggplot(track_monthoverview, aes(x = track_monthoverview$track_name, y = track_monthoverview$totalstreams)) +
+      geom_bar(stat = "identity",  fill = "tomato2", width = 0.6 ) +
+      labs(title = "Most streamed song", x = "Track", y = "Spotify Popularity Ranking") +
       theme(plot.title = element_text(size=14,hjust=-.3,face = "bold"), axis.title = element_text(size=12)) +
-      geom_text(aes(label= artist_popularity), hjust = 1, size = 2, color = 'white') +
+      geom_text(aes(label= totalstreams), hjust = 1, size = 2, color = 'white') +
       coord_flip()
   })
   
